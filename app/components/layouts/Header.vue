f900812bb7d080a808df8bb28a1ab9317b6015c1
AmirGT <25551672+AmirOVH@users.noreply.github.com>
2026-01-01 21:25:07 +0000

    test03

--- app/components/layouts/Header.vue
+++ app/components/layouts/Header.vue
@@ -111,6 +111,10 @@ const { title, github } = useAppConfig()
                   md:w-auto md:rounded-full md:px-3 md:focus:ring-2
                 "
               >
+                <GitHubIcon
+                  class="mr-1 h-5 w-5"
+                />
+                GitHub</a>
 
               <SwitchTheme />
             </div>
