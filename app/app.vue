be223672660abc5bdeea4f5ed77482f64e0f18a0
AmirGT <25551672+AmirOVH@users.noreply.github.com>
2026-01-01 22:46:33 +0000

    test03

--- app/app.vue
+++ app/app.vue
@@ -2,7 +2,7 @@
 const { title, description, image } = useAppConfig()
 
 useSeoMeta({
-  title: `${title} - ${description}`,
+  title: `${title}`,
   description,
   ogType: 'website',
   ogTitle: title,
